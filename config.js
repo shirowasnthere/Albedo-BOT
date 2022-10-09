ef7315f7a24c11a53008bafe1af07f26f699040f
shirowasnthere <100198607+shirowasnthere@users.noreply.github.com>
2022-10-09 12:42:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -49,8 +49,8 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
 
-global.sig = 'https://instagram.com/inizenscuy' // ig
-global.sgh = 'https://github.com/RaaaGH/' // github
+global.sig = 'https://instagram.com/saintyyards' // ig
+global.sgh = 'https://github.com/shirowasnthere/' // github
 global.sgc = 'https://chat.whatsapp.com/' // group whatsapp
 global.sdc = 'https://www.discord.com'//discord
 global.snh = 'https://nhentai.net/g/767842' //Hmmm, Ini boleh kalian isi terserah :v
@@ -58,23 +58,23 @@ global.sfb = 'https://www.facebook.com/' // facebook
 global.syt = 'https://youtube.com/channel/UCTVeRHM0v0Hqv_-O8yVkFpw' // my youtube channel "kalau ada yt ke 2 tarok link nya disini"
 
 /*============== PAYMENT ==============*/
-global.pdana = '62895604670507' //dana
-global.povo = '62895604670507' //ovo
-global.pgopay = '62895604670507' //gopay
-global.plinkaja = '62895604670507' //link aja
-global.ppulsa = '62895604670507' //telkomsel
-global.ppulsa2 = '62895604670507' // kalau ada kartu lain isi aja
-global.psaweria = 'https://saweria.com/'
+global.pdana = '6285750867996' //dana
+global.povo = '6285750867996' //ovo
+global.pgopay = '6285750867996' //gopay
+global.plinkaja = '6285750867996' //link aja
+global.ppulsa = '6285750867996' //telkomsel
+global.ppulsa2 = '6285750867996' // kalau ada kartu lain isi aja
+global.psaweria = 'https://saweria.co/shiroxd'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '62XXXXXXXXXXX'
-global.nomorown = '62895604670507'
-global.namebot = 'AlbedoBotz'
-global.nameown = 'Zenss'
+global.nomorbot = '6281287699064'
+global.nomorown = '6281287699064'
+global.namebot = 'YAN MD'
+global.nameown = 's6inty'
 
 /*============== Github Thanks To ==============*/
-global.namagithub = 'RaaaGH' // nama github lu
-global.linkgithub = 'github.com/RaaaGH' // tarok link github lu
+global.namagithub = 'shirowasnthere' // nama github lu
+global.linkgithub = 'github.com/shirowasnthere' // tarok link github lu
 
 // klau kgk ada github kasih tanda '-' aja biar kgk error
 
@@ -85,10 +85,10 @@ global.lolkey = 'e54205a4ca2caa368cc067bb' // PUNYA WAHID  8e66d0934cf741bfd2182
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['62895604670507', 'Zenss', true], 
-  ['-', 'ISI KALAU ADA', true],
-  ['-', 'ISI KALAU ADA', true],
-  ['-', 'ISI KALAU ADA', true]
+  ['6285750867996', 's6inty', true], 
+  ['-', '6285750867996', true],
+  ['-', '6285750867996', true],
+  ['-', '6285750867996', true]
     //Ganti jd nomormu sama Namamu
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
@@ -635,8 +635,8 @@ global.htjava = '⫹⫺'    //hiasan Doang :v
 global.hsquere = ['⛶','❏','⫹⫺']
 
 /*============== STICKER WM ==============*/
-global.stickpack = 'Albedo-BOT'
-global.stickauth = `By ©WH-MODS-DEV`
+global.stickpack = 'YAN MD'
+global.stickauth = `s6inty need e kitten -1`
 
 global.multiplier = 38 // The higher, The harder levelup
 
